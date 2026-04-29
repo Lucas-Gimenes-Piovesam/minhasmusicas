d11afea82ddfe993926280768d535643e899b317
lucapneu <lucaspiovesam07@gmail.com>
2026-04-29 14:48:43 -0300

    finalizado

--- src/br/com/alura/minhasmusicas/modelos/Audio.java
+++ src/br/com/alura/minhasmusicas/modelos/Audio.java
@@ -1,4 +1,36 @@
 package br.com.alura.minhasmusicas.modelos;
 
 public class Audio {
+    private String titulo;
+    private int totalReproducoes;
+    private int totalcurtidas;
+    private int classificacao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public int getTotalcurtidas() {
+        return totalcurtidas;
+    }
+
+    public int getClassificacao() {
+        return classificacao;
+    }
+
+    public void curte(){
+        this.totalcurtidas++;
+    }
+    public void reproduz(){
+        this.totalReproducoes++;
+    }
+
 }
